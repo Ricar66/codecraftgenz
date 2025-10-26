11da66243d834d9d9cc4b0af1644f437ec88a5e3
Ricardo Coradini de Marco Moretti <ricardo.moretti@tracbel.com.br>
2025-10-26 20:57:15 -0300

    Corrigindo problema com a imagem de fundo

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/*.svg'],
+  build: {
+    assetsInlineLimit: 0, // Força todos os assets a serem copiados como arquivos separados
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]'
+      }
+    }
+  }
 })
