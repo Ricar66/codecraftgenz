ff2da59aba1f209dee0421d82f8913fc91d10317
Ricardo Coradini de Marco Moretti <ricardocoradini97@gmail.com>
2025-11-24 19:50:15 -0300

    deps: add react-helmet-async, zod, cookie-parser to dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './polyfills.js'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { HelmetProvider } from 'react-helmet-async'
 
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
@@ -13,9 +14,11 @@ import './index.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <HelmetProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </HelmetProvider>
     </BrowserRouter>
   </StrictMode>,
 )
