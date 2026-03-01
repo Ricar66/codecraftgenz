3127995ba79d337abe5b3f49a3509b1b796ecae6
Ricardo Coradini de Marco Moretti <ricardocoradini97@gmail.com>
2026-03-01 11:58:34 -0300

    deploy: update build

---
+++ dist/assets/appModel-ChWjNXiV.js
@@ -0,0 +1,1 @@
+import{a as r}from"./urlSanitize-MTL8k_kh.js";function t(t){const i=t?.image||t?.thumbnail||t?.thumb_url||t?.picture_url||"";return i?r(i):""}function i(r){const t=Number(r?.price);return Number.isFinite(t)?t:0}export{i as a,t as g};
