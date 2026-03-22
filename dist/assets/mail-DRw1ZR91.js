d49bd3f22a6c6eb0d44a1070942b1c62e4b8b12a
Ricardo Coradini de Marco Moretti <ricardocoradini97@gmail.com>
2026-03-22 12:21:31 -0300

    deploy: update build

---
+++ dist/assets/mail-DRw1ZR91.js
@@ -0,0 +1,7 @@
+import{c as e}from"./index-t6w7406W.js";
+/**
+ * @license lucide-react v0.577.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=e("lock",[["rect",{width:"18",height:"11",x:"3",y:"11",rx:"2",ry:"2",key:"1w4ew1"}],["path",{d:"M7 11V7a5 5 0 0 1 10 0v4",key:"fwvmzm"}]]),a=e("mail",[["path",{d:"m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7",key:"132q7q"}],["rect",{x:"2",y:"4",width:"20",height:"16",rx:"2",key:"izxlao"}]]);export{t as L,a as M};
